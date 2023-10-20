07044cf5774bbc94c805b4d0f8de5315aefe693b
credendim <130668967+credendim@users.noreply.github.com>
2023-10-20 15:22:30 -0300

    commit

--- Aula 23 - Projeto Blog em PHP/Projeto Blog em PHP/contato.php
+++ Aula 23 - Projeto Blog em PHP/Projeto Blog em PHP/contato.php
@@ -13,7 +13,45 @@
 </head>
 <body>
   <h1>Página de contato</h1>
-</body>
+  <div>
+    <div>
+      <form action="">
+        <input type="text" placeholder="Nome" required>
+        <input type="text" id="telefone" placeholder="Número" required>
+        <input type="email" placeholder="E-mail" required>
+        <textarea name="mensagem" id="mensagem" cols="30" rows="4"></textarea>
+        <button type="submit">Enviar</button>
+      </form>
+      <script>
+
+        const telefone = document.querySelector('#telefone');
+
+        telefone.addEventListener('input', function (event) {
+          const numero = event.target.value;
+          const numeros = numero.replace(/\D/g, '');
+          const parte1 = numeros.slice(0, 2);
+          const parte2 = numeros.slice(2, 4);
+          const parte3 = numeros.slice(4, 9);
+          const parte4 = numeros.slice(9, 13);
+          const numeroFormatado = `(${parte1}) ${parte2} ${parte3}-${parte4}`;
+          event.target.value = numeroFormatado;
+        });
+
+      </script>
+    </div>
+    <div>
+      <h3>Telefone</h3>
+      <p>(55) 11 4002-8922</p>
+      <h3>E-mail</h3>
+      <p>emailexample@gmail.com</p>
+      <h3>Intuito</h3>
+      <p>A página de contato serve para que o usuário possa se comunicar conosco de forma fácil e rápida. Na sua mensagem você pode escrever sobre:</p>
+      <li>Algum feedback</li>
+      <li>Algum erro com o site</li>
+      <li>Dificuldade</li>
+      <li>Segerir alguma serie</li>
+    </div>
+  </div>
 <?php
   include_once("templates/footer.php")
 ?>
