13cb82a6d318a18a209c0d4d013aeb50e6ee18e9
credendim <130668967+credendim@users.noreply.github.com>
2023-10-26 09:55:11 -0300

    commit

---
+++ nosso/data/posts.php
@@ -0,0 +1,32 @@
+<?php
+
+  $posts = [
+    [
+      'id' => 1,
+      'title' => 'Programando em PHP',
+      'description' => 'Neste post você vai aprender tudo que precisa para ser um bom programador de PHP!',
+      'tags' => ['programação', 'PHP', 'lógica de programação'],
+      'img' => 'prog-1.jpg'
+    ],
+    [
+      'id' => 2,
+      'title' => 'Dica de streamer para assinar',
+      'description' => 'Aqui mostraremos os melhores streamer para voce assinar ',
+      'tags' => ['Netflix', 'OOP', 'Programação Avançada'],
+      'img' => 'prog-2.jpg'
+    ],
+    [
+      'id' => 3,
+      'title' => 'A importância da lógica de programação',
+      'description' => 'A lógica de programação é o ponto inicial de muitos estudantes de programação, será que é importante?',
+      'tags' => ['lógica', 'programação', 'algoritmos'],
+      'img' => 'prog-3.jpg',
+    ],
+    [
+      'id' => 4,
+      'title' => 'Programando em PHP',
+      'description' => 'Neste post você vai aprender tudo que precisa para ser um bom programador de PHP!',
+      'tags' => ['programação', 'PHP', 'lógica de programação'],
+      'img' => 'prog-4.jpg'
+    ],
+  ];
