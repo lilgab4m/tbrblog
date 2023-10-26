40462da8beb90d0422a507a175e1eed45610c971
trajano7 <144927103+trajaa7@users.noreply.github.com>
2023-10-26 08:59:52 -0300

    commit

--- nosso/index.php
+++ nosso/index.php
@@ -18,9 +18,11 @@
         <?php
             // Simulação de posts do blog
             $posts = array(
-                array('id' => 1, 'titulo' => 'Post 1', 'conteudo' => 'Conteúdo do post 1.'),
-                array('id' => 2, 'titulo' => 'Post 2', 'conteudo' => 'Conteúdo do post 2.'),
-                array('id' => 3, 'titulo' => 'Post 3', 'conteudo' => 'Conteúdo do post 3.')
+                array('id' => 1, 'titulo' => 'Comédia', 'conteudo' => 'Conteúdo do post 1.'),
+                array('id' => 2, 'titulo' => 'Aventura e Ação', 'conteudo' => 'Conteúdo do post 2.'),
+                array('id' => 3, 'titulo' => 'Terror', 'conteudo' => 'Conteúdo do post 3.'),
+                array('id' => 3, 'titulo' => 'Romance', 'conteudo' => 'Conteúdo do post 3.') ,
+                array('id' => 3, 'titulo' => 'Animação', 'conteudo' => 'Conteúdo do post 3.') ,
             );
 
             // Exibir os posts
